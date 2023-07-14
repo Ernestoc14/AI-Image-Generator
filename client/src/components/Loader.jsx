43e11f9f01872fe2dd3eef1068055d814d9133fa
Ernestoc14 <descreed34@hotmail.com>
2023-07-14 00:49:29 -0500

    Feat Home Page Added

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Spinner, Box } from '@chakra-ui/react';
 
-const Loader = () => {
-    return (
-        <div>Loader</div>
-    )
-}
+const Loader = () => (
+    <Box className='mt-20'>
+        <Spinner
+            thickness="4px"
+            w="50px"
+            h="50px"
+            speed="0.65s"
+            emptyColor="gray"
+            color="#6469ff"
+            size="xl"
+        />
+    </Box>
+);
 
-export default Loader
+export default Loader;
