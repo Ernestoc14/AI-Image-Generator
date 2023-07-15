6dfd2601b09ce6c85555d1663ca58f19bbbcddbe
Ernestoc14 <descreed34@hotmail.com>
2023-07-15 01:15:58 -0500

    Feat Styling FormField Component

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormField = ({ lableName, type, name, placeholder value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
+const FormField = ({ lableName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
     return (
         <div>
             <div className='flex items-center gap-2 mb-2'>
@@ -8,8 +8,26 @@ const FormField = ({ lableName, type, name, placeholder value, handleChange, isS
                     className='block text-sm font-medium text-gray-900'>
                         {lableName}
                     </label>
-                    
+                    {isSurpriseMe && (
+                        <button
+                            type='button'
+                            onClick={handleSurpriseMe}
+                            className='text-sm font-semibold bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'>
+                                Surprise Me
+                            </button>
+                    )}
             </div>
+            <input
+                type={type}
+                id={name}
+                name={name}
+                placeholder={placeholder}
+                value={value}
+                onChange={handleChange}
+                required
+                className='bg-gray-50 border border-gray-300 text-gray-900 focus:ring-[#4649ff]
+                focus:border-[#4649ff] outline-none block w-full rounded-lg text-sm p-3'
+            />
         </div>
     )
 }
