3055ad661ed9d12f2c77ae821aa46b3a86e69f7e
Ernestoc14 <descreed34@hotmail.com>
2023-07-14 23:58:59 -0500

    Feat Creating FormField

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({ lableName, type, name, placeholder value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
     return (
-        <div>FormField</div>
+        <div>
+            <div className='flex items-center gap-2 mb-2'>
+                <label htmlFor={name}
+                    className='block text-sm font-medium text-gray-900'>
+                        {lableName}
+                    </label>
+                    
+            </div>
+        </div>
     )
 }
 
