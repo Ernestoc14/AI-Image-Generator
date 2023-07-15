069c0d258510afe392032658792294b11e83f6e2
Ernestoc14 <descreed34@hotmail.com>
2023-07-15 02:21:55 -0500

    Fixing some Bugs on  Utils and FormField

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-const FormField = ({ lableName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
+const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
     return (
         <div>
             <div className='flex items-center gap-2 mb-2'>
                 <label htmlFor={name}
                     className='block text-sm font-medium text-gray-900'>
-                        {lableName}
-                    </label>
-                    {isSurpriseMe && (
-                        <button
-                            type='button'
-                            onClick={handleSurpriseMe}
-                            className='text-sm font-semibold bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'>
-                                Surprise Me
-                            </button>
-                    )}
+                    {labelName}
+                </label>
+                {isSurpriseMe && (
+                    <button
+                        type='button'
+                        onClick={handleSurpriseMe}
+                        className='text-sm font-semibold bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'>
+                        Surprise Me
+                    </button>
+                )}
             </div>
             <input
                 type={type}
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,4 @@
-import { surpriseMePrompts } from '../constants/prompts';
+import { surpriseMePrompts } from "../constants";
 
 export const getRandomPrompt = (prompt) => {
     const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
