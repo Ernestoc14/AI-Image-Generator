95aa47fb2299efa686fa78ce2fe9e6867c1543a3
Ernestoc14 <descreed34@hotmail.com>
2023-07-15 23:57:02 -0500

    Feat Logic to Create Post Page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,6 +15,10 @@ const CreatePost = () => {
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading] = useState(false);
 
+    const generateImage = () => {
+
+    }
+
     const handleSubmit = () => {
 
     }
@@ -35,7 +39,7 @@ const CreatePost = () => {
                 <p className='mt-2 text-[#666e75] text-[14px] max-w[500px]'>Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>
             </div>
             <form onSubmit={handleSubmit} className='mt-16 max-w-3xl'>
-                <div className='flex items-center'>
+                <div className='flex flex-col gap-5'>
                     <FormField
                         labelName='Your Name'
                         type='text'
@@ -54,7 +58,9 @@ const CreatePost = () => {
                         isSurpriseMe
                         handleSurpriseMe={handleSurpriseMe}
                     />
-                    <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+                    <div className='relative  flex justify-center items-center bg-gray-50 border border-gray-300 text-gray-900
+                        text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 h-64 p-3'
+                    >
                         {form.photo ? (
                             <img 
                                 src={form.photo}
@@ -68,7 +74,24 @@ const CreatePost = () => {
                                 className='w-9/12 h-9/12 object-contain opacity-40'
                             />
                         )}
+                        {generatingImg && (
+                            <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                                <Loader />
+                            </div>
+                        )}
                     </div>
+                </div>
+                <div className='mt-6 gap-5 flex'>
+                    <button
+                        type='submit'
+                        onClick={generateImage}
+                        className=' text-white bg-slate-800 hover:bg-slate-700 w-full py-3 rounded-md shadow-lg font-medium '
+                    >
+                        {generatingImg ? 'Generating...' : 'Generate Image'}
+                    </button>
+                </div>
+                <div>
+                    <p>Once you have created the Image, you can share it in the community with others.</p>
                 </div>
             </form>
         </section>
